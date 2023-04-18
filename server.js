8f188f557992922ac0484c7da9fda566c8d7d074
kshitij2022 <kshitijwohlig@gmail.com>
2023-04-18 23:21:27 +0530

    dsf

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 // Express
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
-console.log(cors,"here");
+console.log(cors, "here");
 
 const app = express();
 
 // app.use(cors({
 //     origin: '*'
 //   }));
-  app.use(cors());
-  // app.use(cors({
-  //     origin: '*',
-  //     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  //     allowedHeaders: ['Content-Type', 'Authorization']
-  //   }));
+app.use(cors());
+// app.use(cors({
+//     origin: '*',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization']
+//   }));
 // app.use((req, res, next) => {
 //     res.setHeader('Access-Control-Allow-Origin', '*');
 //     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -24,28 +24,22 @@ const app = express();
 //   });
 app.use(morgan("tiny"));
 
-
-
-const products = require('./routes/routes')
-const search = require('./routes/searchRoute')
+const products = require("./routes/routes");
+const search = require("./routes/searchRoute");
 // router
 app.use(express.json());
-app.use('/api/v1/',products);
-app.use('/api/v1/',search);
-
-
-  
+app.use("/api/v1/", products);
+app.use("/api/v1/", search);
 
 // dotenv
-const dotenv = require('dotenv');
-dotenv.config({path:"./.env"})
-const port = process.env.PORT 
-
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+const port = process.env.PORT || 3200;
 
 // database
 require("./config/conn");
 
 // listening
-app.listen(port,()=>{
-    console.log(`connected to the ${port}`);
-})  
+app.listen(port, () => {
+  console.log(`connected to the ${port}`);
+});
